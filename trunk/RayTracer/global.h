b8fd15b97de828a7858dbcbac6db33cc4c5fc11f
fbzisme <fbzisme@a1d5ede0-d96e-11de-b50f-7b9694e7f7ce>
2009-11-25 14:18:40 +0000

    fix to 3_recursive.

--- trunk/RayTracer/global.h
+++ trunk/RayTracer/global.h
@@ -8,6 +8,10 @@
 #define MAX_WIDTH 1280
 #define MAX_HEIGHT 800
 #define INF_DISTANCE 100.0f
-#define SHAPE_COUNT 5
+#define SHAPE_COUNT 7
 #define SECT_MIN_DISTANCE 0.01f
+
+#define ENABLE_AMBIENT
+#define ENABLE_DIFFUSE
+#define ENABLE_REFLECT
 
